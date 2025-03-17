1522bd714a03c5ac374713bed4cda594fee0c2f2
jeevships <dhaliwalrajiv@gmail.com>
2025-03-17 12:39:11 -0700

    exercises 2.6-2.9

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    // Check if the name already exists in the phonebook
+    if (persons.some((person) => person.name === newName)) {
+      // Alert the user that the name already exists
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with{' '}
+        <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
+
+      <h3>add a new</h3>
+      <form onSubmit={addPerson}>
+        <div>
+          name:{' '}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          number:{' '}
+          <input
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {filteredPersons.map((person) => (
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
