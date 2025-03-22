9a94fccf142e2d327076f6cc6365e817a8490305
jeevships <dhaliwalrajiv@gmail.com>
2025-03-22 12:28:01 -0700

    1.6: unicafe step 1

--- part1-redo/unicafe/src/App.jsx
+++ part1-redo/unicafe/src/App.jsx
@@ -9,21 +9,31 @@ const Header = () => {
   )
 }
 
+const Total = ({good, neutral, bad}) => {
+  return(
+    <div>
+        all {good + neutral + bad}
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleClick = () => {
-    console.log('clicked')
-  }
-
-
   return (
     <div>
       <Header />
-      <button onClick={handleClick}>good</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>Statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <Total good={good} neutral={neutral} bad={bad}/>
 
     </div>
   )
