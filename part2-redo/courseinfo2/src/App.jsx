3e996fb2fd4a8187f266bbde7162f15fa83b4113
jeevships <dhaliwalrajiv@gmail.com>
2025-03-24 11:00:58 -0700

    2.1: Course information step 6

---
+++ part2-redo/courseinfo2/src/App.jsx
@@ -0,0 +1,66 @@
+const Course = ({course}) => {
+  return(
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      {parts.map(part =>
+        <Part
+        key={part.id}
+        name={part.name}
+        exercises={part.exercises}
+        />
+      )}
+    </div>
+  )
+}
+
+const Part = ({name, exercises}) =>{
+  return(
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
